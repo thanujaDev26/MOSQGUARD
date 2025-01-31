4cc161beeb074abc804e78e9b7f5d8540dd436fb
Taleesha Sandeepa <taleeshasandeepamax@gmail.com>
2025-01-31 09:38:53 +0530

    Update Status.dart

--- lib/pages/Status/Status.dart
+++ lib/pages/Status/Status.dart
@@ -3,6 +3,8 @@ import 'package:mosqguard/pages/Appbar/Header.dart';
 import 'package:mosqguard/pages/Appbar/Footer.dart';
 import 'package:mosqguard/pages/Status/Status_body.dart';
 import 'package:mosqguard/pages/Appbar/Drawer.dart';
+import 'package:mosqguard/pages/sidebar/sidebar.dart';
+import 'package:mosqguard/pages/sidebar/sidebar.dart';
 
 class Status extends StatelessWidget {
   const Status({super.key});
@@ -10,10 +12,37 @@ class Status extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          // leading: Builder(
+          //   builder: (context) => IconButton(
+          //     icon: Icon(Icons.menu, color: Colors.black, size: 30),
+          //     onPressed: () {
+          //       Scaffold.of(context).openDrawer();
+          //     },
+          //   ),
+          // ),
+          // title: Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Image.asset(
+          //       'assets/mosqguard/Logo-3.png',
+          //       width: 150,
+          //     ),
+          //   ],
+          // ),
+          // centerTitle: true,
+          // actions: [
+          //   IconButton(
+          //     icon: Icon(Icons.list, color: Colors.black, size: 30),
+          //     onPressed: () {},
+          //   ),
+          //   SizedBox(width: 10),
+          // ],
+        ),
       body: StatusBody(),
-      drawer: CustomDrawer(), // The Sidebar is now connected here
-      bottomNavigationBar: CustomBottomBar(),
     );
   }
 }
