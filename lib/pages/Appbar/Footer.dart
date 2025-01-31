1ec7a42dc871f9860f9f787e70f032c84c415ccf
ksdkaveesha <149872713+ksdkaveesha@users.noreply.github.com>
2025-01-31 10:31:25 +0530

    page structured

--- lib/pages/Appbar/Footer.dart
+++ lib/pages/Appbar/Footer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:mosqguard/pages/Status/Status.dart';
+import '../Status/Status.dart';
 import '../capture/capture.dart';
-import 'package:mosqguard/pages/notification/Notification.dart';
+import '../notification/Notification.dart';
 
 class CustomBottomBar extends StatelessWidget {
   const CustomBottomBar({super.key});
@@ -24,19 +24,11 @@ class CustomBottomBar extends StatelessWidget {
                     child: Icon(Icons.grid_view)),
                 label: ""),
             BottomNavigationBarItem(
-              icon: GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Status()));
-                },
-                child: Container(
-                  margin: EdgeInsets.only(top: 10),
-                  child: Icon(
-                    Icons.bug_report,
-                  ),
-                ),
-              ),
-              label: "",
+                icon: GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Status()));
+                    },
+                    child: Icon(Icons.bug_report)), label: ""
             ),
             BottomNavigationBarItem(
               icon: GestureDetector(
