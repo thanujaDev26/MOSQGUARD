6990617a6d649403e4e0989e53ca4981f0b60f01
Codersauruse <shehandesk123@gmail.com>
2025-02-02 15:26:03 +0530

    version1 of profilepage added

--- lib/pages/Appbar/Footer.dart
+++ lib/pages/Appbar/Footer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../capture/capture.dart';
+import '../profile/mainProfilePage.dart';
 
 class CustomBottomBar extends StatelessWidget {
   @override
@@ -14,26 +15,45 @@ class CustomBottomBar extends StatelessWidget {
           items: [
             BottomNavigationBarItem(
                 icon: GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       print("object");
                     },
-                    child: Icon(Icons.grid_view)), label: ""
-            ),
+                    child: Icon(Icons.grid_view)),
+                label: ""),
             BottomNavigationBarItem(icon: Icon(Icons.bug_report), label: ""),
             BottomNavigationBarItem(
               icon: GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Capture()));
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Capture()));
                 },
                 child: Container(
                   margin: EdgeInsets.only(top: 10),
-                  child: Icon(Icons.add_circle, size: 70, color: Color(0xff002353),),
+                  child: Icon(
+                    Icons.add_circle,
+                    size: 70,
+                    color: Color(0xff002353),
+                  ),
                 ),
               ),
               label: "",
             ),
             BottomNavigationBarItem(icon: Icon(Icons.notifications), label: ""),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+            BottomNavigationBarItem(
+              icon: GestureDetector(
+                onTap: () {
+                  // Navigate to mainprofile.dart when the person icon is tapped
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            MainProfilePage()), // Navigate to mainprofile
+                  );
+                },
+                child: Icon(Icons.person),
+              ),
+              label: "",
+            ),
           ],
         ),
       ],
